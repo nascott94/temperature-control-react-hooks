35b4f3c0a2acea38c6d2fec09cc4c0f383477437
nascott <nascott94@outlook.com>
2022-02-17 11:19:48 -0700

    simple app using usestate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const App = () => {
-  return <div>hi joji hi</div>;
+  //usestate to set value and color od temperature
+  const [temperatureValue, setTemperatureValue] = useState(50);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 100) return;
+    const newTemperature = temperatureValue + 1;
+
+    setTemperatureValue(newTemperature);
+    if (newTemperature >= 59) {
+      setTemperatureColor('hot');
+    }
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+    const newTemperature = temperatureValue - 1;
+
+    setTemperatureValue(newTemperature);
+    if (newTemperature < 68) {
+      setTemperatureColor('cold');
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°F
+        </div>
+      </div>
+      <div className="button-container">
+        <button onMouseDown={increaseTemperature}>+</button>
+        <button onClick={decreaseTemperature}>-</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
